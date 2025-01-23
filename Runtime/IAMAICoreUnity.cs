7550a6c3f24e83c86e7527981f779f9a3663aa66
Troik-Nardimos <105459664+TroikNardimos@users.noreply.github.com>
2025-01-23 13:32:46 -0700

    Added plugin

---
+++ Runtime/IAMAICoreUnity.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace IAMAICore.Unity
+{
+    public class IAMAICoreUnity : MonoBehaviour
+    {
+        string Generate(string input)
+        {
+            return input;
+        }
+    }
+}
