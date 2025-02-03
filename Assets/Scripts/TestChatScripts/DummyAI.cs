7a74fecbe6d19db4966f96bd4d76f028943f711c
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-02-03 16:42:14 -0700

    Finally got the plugin working

--- Assets/Scripts/TestChatScripts/DummyAI.cs
+++ Assets/Scripts/TestChatScripts/DummyAI.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-using IAMAICore.Unity;
+using iamai_core_lib;
 
 public class DummyAI : MonoBehaviour
 {
@@ -10,16 +10,16 @@ public class DummyAI : MonoBehaviour
     {
         // Replace this with real AI logic or API call
         //string aiResponse = $"You said: {userMessage}";
-        IAMAICoreUnity iAMAICoreUnity = new IAMAICoreUnity();
+        iamai_core_lib.AI ai = new iamai_core_lib.AI("llama-3.2-1b-instruct-q4_k_m.gguf");
 
         // Add the AI's response after a delay
-        StartCoroutine(DelayedAIResponse("You said: " + iAMAICoreUnity.Generate(userMessage)));
+        StartCoroutine(DelayedAIResponse(ai.Generate(userMessage)));
         //chatController.AddMessage(aiResponse, false);
     }
 
     private IEnumerator DelayedAIResponse(string aiResponse)
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.0f);
         chatController.AddMessage(aiResponse, false);
     }
 }
