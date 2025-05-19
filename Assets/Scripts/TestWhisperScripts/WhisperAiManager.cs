340a76e4af5bf602de33ad0b956e99342126334c
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2025-05-19 14:27:01 -0600

    add functions

--- Assets/Scripts/TestWhisperScripts/WhisperAiManager.cs
+++ Assets/Scripts/TestWhisperScripts/WhisperAiManager.cs
@@ -2,16 +2,17 @@ using iamai_core_lib;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class WhisperAiManager : MonoBehaviour
 {
 	public static WhisperAiManager Instance { get; private set; }
-
+	public TextMeshProUGUI outputText;
 	public List<string> WhisperModelList;
 
 	private iamai_core_lib.AI ai;
-
+	public float[] pcmData;
 	bool recording = false;
 
 	private void Awake() {
@@ -27,13 +28,20 @@ public class WhisperAiManager : MonoBehaviour
 		}
 	}
 
-	public void onClick() {
+	public async void onClick() {
 		if (recording) {
 			recording = false;
 			MicrophoneInput.instance.StopRecording();
 			//change audio source clip to pcm32
-			// await ai. transcribe
+			AudioClip clip = MicrophoneInput.instance.audioSource.clip;
+			int samples = clip.samples;
+			int channels = clip.channels;
+			pcmData = new float[samples* channels];
+			clip.GetData(pcmData, 0);
+			//await ai. transcribe
+			string output = await ai.WhisperAsyncTranscribe(pcmData, samples);
 			//set output text to transcribed text
+			outputText.text = output;
 		} else {
 			recording = true;
 			MicrophoneInput.instance.StartRecording();
